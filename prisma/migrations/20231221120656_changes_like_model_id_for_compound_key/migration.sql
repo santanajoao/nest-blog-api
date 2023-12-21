cb9f6dd9b86b0983ce79d34252eb3947ecd707a2
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-12-21 14:59:07 -0300

    chore: change like model id for compound key

---
+++ prisma/migrations/20231221120656_changes_like_model_id_for_compound_key/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - The primary key for the `Like` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `id` on the `Like` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Like` DROP PRIMARY KEY,
+    DROP COLUMN `id`,
+    ADD PRIMARY KEY (`userId`, `postId`);
